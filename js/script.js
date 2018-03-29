d01846851ecb1d5c279aa96d3e78e65e32bceb38
Alen <casualen@gmail.com>
2018-03-29 16:33:21 -0400

    .

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var link = document.querySelector(".search-top");
+var popup = document.querySelector(".search-hotel-form");
+
+link.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.add("search-show");
+  });
+
+
+
+
